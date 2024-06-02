73fe8d32b980a158e901975196eed0c20ca67575
Jason Joannou <jjoannou@bscglobal.com>
2024-06-03 00:10:53 +0200

    Added some home page changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='relative'>
-      <div className='absolute left-[100px]'>
-        <h1 className='font-bold text-[250px] text-white' style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>HOCA</h1>
+    <div className="flex items-center justify-center h-5/6 relative">
+      <div className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 className="font-bold text-[250px] text-white ml-[30px]" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)' }}>HOCA</h1>
       </div>
-      <div className='absolute left-[120px] mt-[310px]'>
-        <p className='text-[40px] text-white'>Hellenic Outreach and Community Association</p>
-        <p className='text-[20px] text-white'>The Hellenic Outreach and Community Association (HOCA) is dedicated to fostering unity, respect, and cultural preservation among Greek youth. 
-        <br/>By promoting the values of Philotimo—honor, integrity, and selflessness—HOCA nurtures future leaders who embrace their heritage. 
-        <br/>Through workshops, mentorship, and outreach, HOCA equips youth with the skills to face modern challenges while staying rooted in their cultural identity.</p>
+      <div className="absolute left-[650px] mb-10">
+        <p className="text-[40px] text-white">Hellenic Outreach and Community Association</p>
+      </div>
+      <div className="absolute top-[400px] left-[810px] flex space-x-10">
+        <button className="text-[24px] text-white rounded-full bg-gradient-to-t from-[#00b1a3] to-[#42cdc2] px-8 py-3 shadow-lg hover:from-red-500 hover:to-red-700 transition ease-in-out duration-300">About</button>
+        <button className="text-[24px] text-white rounded-full bg-gradient-to-t from-[#00b1a3] to-[#42cdc2] px-8 py-3 shadow-lg hover:from-red-500 hover:to-red-700 transition ease-in-out duration-300">Sign Up!</button>
       </div>
     </div>
   )
