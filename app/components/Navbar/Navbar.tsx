4093b58f7fcc3e3a6c6137ffeeae2b8e0a16e566
Jason Joannou <jjoannou@bscglobal.com>
2024-06-03 00:10:34 +0200

    Added some changes to navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -5,14 +5,14 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <header className='bg-gradient-to-b from-[#eeefac] via-[#a6efe9] to-[#a6efe9] p-4'>
-        <nav className="flex justify-between items-center w-[92%] mx-auto pt-4">
+        <nav className="flex flex-wrap justify-between items-center w-[92%] mx-auto pt-4">
             <div>
                 <Link href="/" className='text-white text-xl font-bold'>
                     HOCA
                 </Link>
             </div>
-            <div className="">
-                <ul className="flex items-center gap-[4vw]">
+            <div className="flex-grow">
+                <ul className="flex flex-wrap justify-center items-center gap-4 md:gap-8 lg:gap-8 ml-[120px]">
                 <li>
                     <Link href="/about" className="text-white hover:text-gray-500">About</Link>
                 </li>
@@ -30,7 +30,7 @@ const Navbar = () => {
                 </li>
                 </ul>
             </div>
-            <div>
+            <div className='flex space-x-2'>
                 <button className='bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec] mr-2'>Sign up</button>
                 <button className='bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]'>Login</button>
             </div>
